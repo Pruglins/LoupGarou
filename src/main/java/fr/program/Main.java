4347f690962f880369c0269ff6d2406c4ee9b054
Program132 <clement.gauthier132@orange.fr>
2023-06-09 19:46:22 +0200

    Update Main.java

--- src/main/java/fr/program/Main.java
+++ src/main/java/fr/program/Main.java
@@ -1,5 +1,7 @@
 package fr.program;
 
+import fr.program.cmds.PartieCMD;
+import fr.program.cmds.RolesCMD;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -7,6 +9,13 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("[LOUP GAROU] Enabled.");
+
+        this.saveDefaultConfig();
+        this.saveResource("compos_jeu.roles", true);
+        this.saveResource("compos_jeu.nb_roles", true);
+
+        this.getCommand("partie").setExecutor(new PartieCMD(this));
+        this.getCommand("roles").setExecutor(new RolesCMD(this));
     }
 
     @Override
