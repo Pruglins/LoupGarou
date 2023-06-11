ad1b9430135eac31b82511f529cb309429b7ef79
Program132 <clement.gauthier132@orange.fr>
2023-06-11 19:10:25 +0200

    Moved Cupidon Interact

--- src/main/java/fr/program/EventsListener.java
+++ src/main/java/fr/program/EventsListener.java
@@ -1,11 +1,14 @@
 package fr.program;
 
+import fr.program.events.Cupidon;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class EventsListener implements Listener {
 
@@ -26,7 +29,7 @@ public class EventsListener implements Listener {
         String role_player = config.getString("partie.joueurs." + p.getName());
 
         if (currentGameRole.equals("Cupidon") && role_player.equals("Cupidon")) {
-
+            Cupidon.cupidon_interaction(inv, event, p, config);
         }
     }
 }
